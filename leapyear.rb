5fbf97166bd1b92fd5b7157295173552c1dc1527
sutantoc <165302919+sutantoc@users.noreply.github.com>
2024-04-04 04:09:19 +0000

    leapyear

---
+++ leapyear.rb
@@ -0,0 +1,12 @@
+years = [
+  1700,
+  1940,
+  2038
+]
+year = years.sample
+
+if year % 4 == 0 && year % 100 != 0
+  pp "#{year} is a leap year!"
+else
+  pp "#{year} is not a leap year."
+end
---
+++ twofer.rb
@@ -0,0 +1,6 @@
+name = ["raghu", "Bob", ""].sample
+if name != ""
+  pp "One for " + name.capitalize + ", one for me."
+else
+  pp "One for you, one for me."
+end
